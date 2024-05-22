00ad5dcdbed62e59f559cac78e220a6cad6072aa
zukhriddin <zux.828@gmail.com>
2024-05-22 09:22:13 +0500

    add signals

--- backend/apps/main/signals.py
+++ backend/apps/main/signals.py
@@ -1,11 +1,13 @@
-from django.db.models.signals import post_save, post_delete
+from django.db.models.signals import post_save, post_delete, pre_delete
 from django.dispatch import receiver
 from django_celery_beat.models import PeriodicTask
 
 from apps.main.models import Post
 
 
-@receiver(post_delete, sender=Post)
+@receiver(pre_delete, sender=Post)
 def post_delete_signal(sender, instance, **kwargs):
-    post_id = instance.id
-    PeriodicTask.objects.filter(kwargs=f'{{"planned_post_id": {post_id}}}', enabled=True).update(enabled=False)
+    planned_posts_data = []
+    for i in instance.planned_posts.all().values_list('id', flat=True):
+        planned_posts_data.append(f'{{"planned_post_id": {i}}}')
+    PeriodicTask.objects.filter(kwargs__in=planned_posts_data, enabled=True).update(enabled=False)

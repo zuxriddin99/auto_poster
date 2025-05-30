cd71cb554b05ae1ef81d7d75b6616c32a4d4bb50
zukhriddin <zux.828@gmail.com>
2025-05-30 17:38:36 +0500

    a

--- backend/apps/main/tasks.py
+++ backend/apps/main/tasks.py
@@ -11,8 +11,8 @@ import time
 logger = get_task_logger(__name__)
 
 
-@shared_task
-def publish_post(planned_post_id: int):
+@shared_task(bind=True, queue='serial_queue')
+def publish_post(self, planned_post_id: int):
     try:
         planned_post = PlannedPosts.objects.get(id=planned_post_id)
     except PlannedPosts.DoesNotExist:
@@ -21,12 +21,12 @@ def publish_post(planned_post_id: int):
 
     raise_error = False
     for p_post in planned_post.chennals.all():
-        # try:
-        chennal_check_and_delay(p_post.channel_username)
-        update_chennal_last_send_msg(p_post.channel_username)
-        send_posts(chat=p_post.channel_username, post_id=post_id)
-        # except Exception as e:
-        #     capture_exception(e)
-        #     raise_error = True
-    # if raise_error:
-    #     return "Not fully complated"
+        try:
+            chennal_check_and_delay(p_post.channel_username)
+            update_chennal_last_send_msg(p_post.channel_username)
+            send_posts(chat=p_post.channel_username, post_id=post_id)
+        except Exception as e:
+            capture_exception(e)
+            raise_error = True
+    if raise_error:
+        return "Not fully complated"
